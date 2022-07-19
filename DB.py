90652028e03786b696b6c35aa3faeb865a42f362
Muller <[preensikl@gmail.com]>
2022-07-19 19:27:57 +0300

    Total upgrade_19.07

--- DB.py
+++ DB.py
@@ -18,19 +18,11 @@ def table_3_func():
 def parsing_bd():
     conn = mysql.connector.connect(host = 'localhost', user = 'root', passwd = 'zuma057195Z!', db = 'posterposs')
     cursor = conn.cursor()
-    cursor.execute(f'SELECT * FROM summ_count WHERE Tables = 1')
+    cursor.execute(f'SELECT * FROM summ_count WHERE Tables = {peremen}')
     a = (str(cursor.fetchall()).replace('), (', '\n')).strip('[').strip(']').strip('(').strip(')').strip(",").strip("'")
     print(a)
     return a
     conn.close()
-
-# def menu_pars():
-#     conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
-#     cursor = conn.cursor()
-#     cursor.execute('SELECT * FROM menu')
-#     a = str(cursor.fetchall()).replace("), (", '\n').strip('[').strip(']').strip('(').strip(')')
-#     return a
-#     conn.close()
 
 def tables_summ():
     conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
@@ -47,38 +39,6 @@ def clear():
     conn.commit()
     conn.close()
 
-def Americano_add():
-    conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
-    cursor = conn.cursor()
-    cursor.execute(f'INSERT orders (id_sell_table, id_menu, counts) VALUES ({peremen}, 3, 1)')
-    conn.commit()
-    conn.close()
-
-def Latte_add():
-    conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
-    cursor = conn.cursor()
-    cursor.execute(f'INSERT orders (id_sell_table, id_menu, counts) VALUES ({peremen}, 1, 1)')
-    conn.commit()
-    conn.close()
-
-def Cappuccino_add():
-    conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
-    cursor = conn.cursor()
-    cursor.execute(f'INSERT orders (id_sell_table, id_menu, counts) VALUES ({peremen}, 2, 1)')
-    conn.commit()
-    conn.close()
-
-
-# table_3 = tk.Button(text = 'table_3', width = 15, height = 3, command = switch_table_3)
-# table_3.place(x = 270, y = 25)
-
-# class add_drinks():
-#     def __init__(self, peremen):
-#         self.conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
-#         self.cursor = conn.cursor()
-#         self.cursor.execute(f'INSERT orders (id_sell_table, id_menu, counts) VALUES ({peremen}, 2, 1)')
-#         self.conn.commit()
-#         self.conn.close()
 
 def got_count_menu():
     conn = mysql.connector.connect(host='localhost', user='root', passwd='zuma057195Z!', db='posterposs')
