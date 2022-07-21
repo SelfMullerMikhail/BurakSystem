4247443d122afa68daa0e7572b6b6011497c54a4
Muller <[preensikl@gmail.com]>
2022-07-21 15:11:47 +0300

    All there are classes

--- create_menu_botton.py
+++ create_menu_botton.py
@@ -1,4 +1,4 @@
-from DB import db_helper, return_table, got_name, switch_table_bd
+from BD import db_helper, BD
 import tkinter as tk
 from upgrade_all import upgrade
 # from main_window import window
@@ -6,6 +6,7 @@ from upgrade_all import upgrade
 class create_menu_botton():
     helper = db_helper()
     upgrade_class = upgrade()
+    database = BD()
 
     def get_line(self):
         self.line = self.helper.execute_query_fetchone("SELECT max(id) FROM menu")
@@ -14,7 +15,7 @@ class create_menu_botton():
 
     def buttons_create(self, counter, y, x, wind, summ):
         def button_function():
-            table = return_table()
+            table = self.database.return_table()
             self.helper.execute_query_insert(f"INSERT orders (id_sell_table, id_menu, counts) VALUES({table}, {counter}, 1)")
             self.upgrade_class.upgrade_all(wind, summ)
         self.i = tk.Button(text=self.name, command=button_function, width=15, height=3)
@@ -29,7 +30,8 @@ class create_menu_botton():
             self.x = 450
             self.y += 60
             for i in range(3):
-                self.name = got_name(self.counter)
+
+                self.name =self.database.got_name(self.counter)
                 if self.name == "None":
                     break
                 self.x += 130
@@ -37,16 +39,19 @@ class create_menu_botton():
                 self.counter += 1
 
 class create_menegment_botton():
+    database = BD()
     upgrade_class = upgrade()
     def buttons_create(self, number, wind, summ):
         def button_function():
-            switch_table_bd(number)
+            self.database.switch_table_bd(number)
             self.upgrade_class.upgrade_all(wind, summ)
         self.i = tk.Button(text=f"Table {number}", command=button_function, width=15, height=3)
         self.i.grid()
 
     def __init__(self, win, sum):
-        for i in range(5):
+        self.helper = db_helper()
+        self.line = self.helper.execute_query_fetchone("SELECT max(id) FROM sell_table")
+        for i in range(self.line[0]):
             self.buttons_create(i+1, win, sum)
 
 
